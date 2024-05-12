b7610885d358e7e06eba2b07acff782b495b3d4e
Nikolay Podlesnykh <podlesnykh.nikolay@gmail.com>
2024-05-12 16:21:55 +0200

    Initial commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,19 @@
+package url_shortener
+
+import (
+	"fmt"
+	"url-shortener/internal /config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+	// TODO init logger: slog
+
+	// TODO init storage: sqlite
+
+	// TODO init router: chi, "chi render"
+
+	// TODO run server
+}
